3fe66e242695425ced674615c151b28377e8f9e6
danda <daniel.krpelik@durham.ac.uk>
2018-02-21 12:47:34 +0000

    opt1

--- danda/OPT2/MTL/controlEQ.m
+++ danda/OPT2/MTL/controlEQ.m
@@ -1,10 +1,15 @@
 function [u] = controlEQ(t,X)
 
-n = length(X)
+n = length(X);
 
 alfa = X(1);
-cnt = X(2:end)
-u = sqrt(t*alfa) ;
+
+c_vals = X(2:end);
+c_points = linspace(0,1,n-1);
+
+
+u = interp1(c_points, c_vals, t);
+%u = sqrt(t*alfa) ;
 
 
 end
--- danda/OPT2/MTL/test.m
+++ danda/OPT2/MTL/test.m
@@ -1,10 +1,13 @@
-X = [1.];
+
+n_cp = 10;
+
+X = [10., zeros(1,n_cp)];
 df = @(t,state) dfun(t,state,@controlEQ, X);
 eqC = @( parameter ) eqConstrains( parameter );
 
 A = [ 1 ]; b = [1];
-lb = [0];
-ub = [(pi/2)^2];
-x0 = [1];
+lb = [10; -ones(n_cp,1)*(pi/2)^2];
+ub = [1e12; ones(n_cp,1)*(pi/2)^2];
+x0 = [1,zeros(1,n_cp)];
 
-fmincon(@objFun, x0 , [], [], [], [], [], [], @eqConstrains)
+fmincon(@objFun, x0 , [], [], [], [], lb, ub, @eqConstrains)
